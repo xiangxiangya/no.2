88d6b1ce19756e689819d2f142070cde1bcc181d
xiangxiangya <1119188979@qq.com>
2020-03-08 18:23:26 +0800

    ddaw

---
+++ 1/1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  1
+//
+//  Created by 岳浩翔 on 2020/3/8.
+//  Copyright © 2020 岳浩翔. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "This is Mac!\n";
+    return 0;
+}
