4fb0222ae0e5dc0b5a71de90f3b78f67955ede87
xiangxiangya <1119188979@qq.com>
2020-03-08 19:02:26 +0800

    Sure

--- Hashbucket/Hashbucket/main.cpp
+++ Hashbucket/Hashbucket/main.cpp
@@ -1,15 +1,15 @@
 //
 //  main.cpp
-//  Hashbucket
+//  HashBucket
 //
 //  Created by 岳浩翔 on 2020/3/8.
 //  Copyright © 2020 岳浩翔. All rights reserved.
 //
 
 #include <iostream>
-
+#include"HashBucket.hpp"
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "你好！\n";
     return 0;
 }
